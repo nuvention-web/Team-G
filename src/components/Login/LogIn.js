599b106ffd6b396237746568117b7e74da850c71
Siddharth <sgiedne@gmail.com>
2017-02-21 02:00:10 -0600

    Login authentication reactified || Sidd || Feb 21 2017

--- src/components/Login/LogIn.js
+++ src/components/Login/LogIn.js
@@ -4,27 +4,29 @@ import './Login.css'
 import * as firebase from 'firebase';
  
 export default class LogIn extends React.Component {
+  constructor(){
+    super();
+    this.validate = this.validate.bind(this);
+    this.register = this.register.bind(this);
+    this.state = {
+      email: '',
+      pwd: ''
+    }
+  }
 
   loginClicked(e){
     e.preventDefault();
     console.log("HERE")
     this.props.authFunc();
   }
+
 validate() {
-    console.log("hello");
+    console.log("Enter validate");
     const auth = firebase.auth();
-    var id = document.getElementById("txtEmail");
-    var pass = document.getElementById("txtPwd");
-    var email = "";
-    var pwd = "";
-     if(id!=null){
-         email = id.value;
-     }
-    if(pass!=null){     
-         pwd = pass.value;
-    }
-    console.log(email);
-    console.log(pwd);
+    
+    const email = this.state.email;
+    const pwd = this.state.pwd;
+
     const promise = auth.signInWithEmailAndPassword(email,pwd);
    
     promise
@@ -41,19 +43,14 @@ validate() {
        }
     });
 }
+
     register() {
-    console.log("hello");
+    console.log("Enter register");
     const auth = firebase.auth();
-    var id = document.getElementById("txtEmail");
-    var pass = document.getElementById("txtPwd");
-    var email = "";
-    var pwd = "";
-     if(id!=null){
-         email = id.value;
-     }
-    if(pass!=null){     
-         pwd = pass.value;
-    }
+
+    const email = this.state.email;
+    const pwd = this.state.pwd;
+    
     const promise = auth.createUserWithEmailAndPassword(email,pwd);
     promise
         promise
@@ -71,15 +68,29 @@ validate() {
     });
 }
 
+
+setEmail(e){
+  //console.log("emaol" + e.target.value)
+  this.setState({
+    email: e.target.value
+  });
+}
+
+setPwd(e){
+  //console.log("poss" + e.target.value)
+  this.setState({
+    pwd: e.target.value
+  });
+}
+
   render() {
           return (
             <div className="login-card">
               <h1>Log-in</h1><br />
               <form onSubmit={this.loginClicked.bind(this)}>
-                <input type="text" name="user" id="txtEmail" placeholder="Username" />
-                <input type="password" name="pass" id="txtPwd" placeholder="Password" />
+                <input type="text" name="user" id="txtEmail" placeholder="Username" value={ this.state.email } onChange={ this.setEmail.bind(this) }/>
+                <input type="password" name="pass" id="txtPwd" placeholder="Password" value={ this.state.pwd } onChange={this.setPwd.bind(this)}/>
                 <button type="button" className="login login-submit" onClick={this.validate}>Log In</button>
-
               </form> 
               <div className="login-help">
                 <a href="#" onClick={this.register}>Register</a> • <a href="#">Forgot Password</a>
