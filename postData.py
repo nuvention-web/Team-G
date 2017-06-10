f1831ce5a66d7c3448c7a8f0c5ab371ac5591f4d
lyanne228 <lyanneloh2017@u.northwestern.edu>
2017-06-10 08:24:52 -0500

    10 June 2017 || Lyanne
    1) implement send_mail (client side)
    2) add prompts for all functions on postData.py
    3) test all functions on postData.py
    4) find out how to pull data about instagram followers

--- postData.py
+++ postData.py
@@ -5,29 +5,45 @@ url = 'http://murphy.wot.eecs.northwestern.edu/~sme0261/followerstackserv.py'
 
 def main() :
 
-	req_type = sys.argv[1]
-	username = sys.argv[2]
+	print 'Request Types:'
+	print ' - adduser (create a new user)'
+	print ' - getuser (search for user)'
+	print ' - listusers (list all users)'
+	print ' - modifyuser (modify user account data)'
+	print ' - deleteuser (delete a user)\n'
+	print ' - updatesm (update or add a social media account)'
+	print ' - listusersm (list a user\'s social media accounts)'
+	print ' - deletesm (delete a social media account)\n'
+	print ' - addtp (add a user\'s target parameter)'
+	print ' - listusertp (list a user\'s target parameters)'
+	print ' - deletetp (delete a target parameter)\n'
+	print ' - getgrowth (list the follower count for user\'s social media account)'
+	print ' - getpaymentlog (list user\'s past payments)'
+	print ' - getmthpayment (list all payments for a month)'
+	print ' - send_mail (send email reminder to user)'
+	req_type = input('Enter request type: ')
 	
 	# ----------------------------- Account Data -------------------------------
 
-	# Create new user (adduser username password company rep_name email phone active(1/0 T/F) card_number card_cvc card_exp_date reoccuring(1/0 T/F) amount)
+	# Create new user
 	if req_type == 'adduser':
-		password = sys.argv[3]
-		company = sys.argv[4]
-		rep_name = sys.argv[5]
-		email = sys.argv[6]
-		phone = sys.argv[7]
-		active = sys.argv[8]
-		card_number = sys.argv[9]
-		card_cvc = sys.argv[10]
-		card_exp_date = sys.argv[11]
-		reoccuring = sys.argv[12]
-		amount = sys.argv[13]
-		if int(active) == 1:
+		username = input('Username: ')
+		password = input('Password: ')
+		company = input('Company: ')
+		rep_name = input('Representative\'s name: ')
+		email = input('Email: ')
+		phone = input('Phone: ')
+		active = input('Account active? (Y/N): ')
+		card_number = input('Card number: ')
+		card_cvc = input('Card CVC: ')
+		card_exp_date = input('Card expirary date: ')
+		reoccuring = input('Reoccuring payments? (Y/N) : ')
+		amount = input('Monthly amount: USD ')
+		if active == 'Y' or active == 'y':
 			active = True
 		else:
 			active = False
-		if int(reoccuring) == 1:
+		if reoccuring == 'N' or reoccuring == 'n':
 			reoccuring = True
 		else:
 			reoccuring = False
@@ -35,42 +51,88 @@ def main() :
 		payload = {'req_type': req_type, 'username': username, 'password': password, 'company': company, 'rep_name': rep_name, 'email': email, 'phone': phone, 'active': active, 'card_number': card_number, 'card_cvc': card_cvc, 'card_exp_date': card_exp_date, 'reoccuring': reoccuring, 'amount': amount}
 		r = requests.post(url, data=payload)
 		if 'ERROR: User already exists' in r.text:
-			print 'That username already exists, please enter another username'
+			while 'ERROR: User already exists' in r.text:
+				username = input('That username already exists, please enter another username: ')
+				payload = {'req_type': req_type, 'username': username, 'password': password, 'company': company, 'rep_name': rep_name, 'email': email, 'phone': phone, 'active': active, 'card_number': card_number, 'card_cvc': card_cvc, 'card_exp_date': card_exp_date, 'reoccuring': reoccuring, 'amount': amount}
+				r = requests.post(url, data=payload)
 		else:
 			print 'New user account created. Username: '+username
 
-	# Search for user (getuser username)
-	if req_type == 'getuser':
+	# Search for user
+	elif req_type == 'getuser':
+		username = input('Username: ')
 		payload = {'req_type': req_type, 'username': username}
 		r = requests.post(url, data=payload)
 		if 'ERROR' in r.text:
 			print 'No user found'
 		else:
-			print r.text
+			print r.text.split("\n",4)[4]
 
-	# List users (listusers)
+	# List users
 	if req_type == 'listusers':
 		payload = {'req_type': req_type}
 		r = requests.post(url, data=payload)
 		if 'ERROR' in r.text:
 			print 'No users'
 		else:
-			print r.text
+			print r.text.split("\n",4)[4]
 
-	# Modify user (modifyuser username email newEmail@gmail.com)
+	# Modify user
 	if req_type == 'modifyuser':
-		modify_field = sys.argv[3]
-		modify_value = sys.argv[4]
+		username = input('Username: ')
+		print 'User\'s account data fields'
+		print ' 1) password'
+		print ' 2) company'
+		print ' 3) representative\'s name'
+		print ' 4) email'
+		print ' 5) phone'
+		print ' 6) account active'
+		print ' 7) card number'
+		print ' 8) card CVC'
+		print ' 9) card expirary date'
+		print '10) reoccuring payments'
+		print '11) monthly amount'
+
+		modify_field = input('Which field (1-11): ')
+		while modify_field < 1 or modify_field > 11:
+			modify_field = input('Which field (1-11): ')
+		if int(modify_field) == 1:
+			modify_field = 'password'
+		elif int(modify_field) == 2:
+			modify_field = 'company'
+		elif int(modify_field) == 3:
+			modify_field = 'rep_name'
+		elif int(modify_field) == 4:
+			modify_field = 'email'
+		elif int(modify_field) == 5:
+			modify_field = 'phone'
+		elif int(modify_field) == 6:
+			modify_field = 'active'
+			print 'Account active options: Y/N'
+		elif int(modify_field) == 7:
+			modify_field = 'card_number'
+		elif int(modify_field) == 8:
+			modify_field = 'card_cvc'
+		elif int(modify_field) == 9:
+			modify_field = 'card_exp_date'
+		elif int(modify_field) == 10:
+			modify_field = 'reoccuring'
+			print 'Reoccuring payments options: Y/N'
+		elif int(modify_field) == 11:
+			modify_field = 'amount'
+
+		modify_value = input('New field value: ')
 
 		payload = {'req_type': req_type, 'username': username, 'modify_field': modify_field, 'modify_value': modify_value}
 		r = requests.post(url, data=payload)
 		if 'ERROR' in r.text:
-			print 'Incorrect username or fieldname. Account details not modified'
+			print 'Incorrect username. Account details not modified'
 		else:
 			print modify_field+' successfully modified'
 
-	# Delete user (deleteuser username)
+	# Delete user
 	if req_type == 'deleteuser':
+		username = input('Username: ')
 
 		payload = {'req_type': req_type, 'username': username}
 		r = requests.post(url, data=payload)
@@ -83,11 +145,12 @@ def main() :
 
 	# -------------------------- Social Media Account Data ---------------------------
 	
-	# Add/update SM account (updatesm username sm_platform sm_username sm_password)
+	# Add/update SM account
 	if req_type == 'updatesm':
-		sm_platform = sys.argv[3]
-		sm_username = sys.argv[4]
-		sm_password = sys.argv[5]
+		username = input('Username: ')
+		sm_platform = input('Social media platform (Instagram, Twitter, or Facebook): ')
+		sm_username = input('Social media account username: ')
+		sm_password = input('Social media account password: ')
 
 		payload = {'req_type': req_type, 'username': username, 'sm_platform': sm_platform, 'sm_username': sm_username, 'sm_password': sm_password}
 		r = requests.post(url, data=payload)
@@ -96,18 +159,21 @@ def main() :
 		else:
 			print sm_platform+' account updated'
 
-	# List user's SM accounts (listusersm username)
+	# List user's SM accounts
 	if req_type == 'listusersm':
+		username = input('Username: ')
+
 		payload = {'req_type': req_type, 'username': username}
 		r = requests.post(url, data=payload)
 		if 'ERROR' in r.text:
 			print 'No such user exists, or no social media accounts found for this user'
 		else:
-			print r.text
+			print r.text.split("\n",4)[4]
 
-	# Delete SM account (deletesm username sm_platform)
+	# Delete SM account
 	if req_type == 'deletesm':
-		sm_platform = sys.argv[3]
+		username = input('Username: ')
+		sm_platform = input('Social media platform (Instagram, Twitter, or Facebook): ')
 
 		payload = {'req_type': req_type, 'username': username, 'sm_platform': sm_platform}
 		r = requests.post(url, data=payload)
@@ -118,10 +184,11 @@ def main() :
 
 	# --------------------------------- Target Parameters -------------------------------
 
-	# Add target parameter (addtp username param_type param_description)
+	# Add target parameter
 	if req_type == 'addtp':
-		param_type = sys.argv[3]
-		param_description = sys.argv[4]
+		username = input('Username: ')
+		param_type = input('Target parameter type: ')
+		param_description = input('Target parameter description: ')
 
 		payload = {'req_type': req_type, 'username': username, 'param_type': param_type, 'param_description': param_description}
 		r = requests.post(url, data=payload)
@@ -130,19 +197,22 @@ def main() :
 		else:
 			print 'Target parameter added'
 
-	# List user's target parameters (listusertp username)
+	# List user's target parameters
 	if req_type == 'listusertp':
+		username = input('Username: ')
+
 		payload = {'req_type': req_type, 'username': username}
 		r = requests.post(url, data=payload)
 		if 'ERROR' in r.text:
 			print 'No such user exists, or no target parameters found for this user'
 		else:
-			print r.text
+			print r.text.split("\n",4)[4]
 
-	# Delete target parameter (deletetp username param_type param_description)
+	# Delete target parameter
 	if req_type == 'deletetp':
-		param_type = sys.argv[3]
-		param_description = sys.argv[4]
+		username = input('Username: ')
+		param_type = input('Target parameter type: ')
+		param_description = input('Target parameter description: ')
 
 		payload = {'req_type': req_type, 'username': username, 'param_type': param_type, 'param_description': param_description}
 		r = requests.post(url, data=payload)
@@ -153,39 +223,56 @@ def main() :
 
 	# --------------------------- Social Media Account Growth ---------------------------
 
-	# List user's social media account growth (getgrowth username sm_platform)
+	# List user's social media account growth
 	if req_type == 'getgrowth':
-		sm_platform = sys.argv[3]
+		username = input('Username: ')
+		sm_platform = input('Social media platform (Instagram, Twitter, or Facebook): ')
 
 		payload = {'req_type': req_type, 'username': username, 'sm_platform': sm_platform}
 		r = requests.post(url, data=payload)
 		if 'ERROR' in r.text:
 			print 'Incorrect username or sm_platform'
 		else:
-			print r.text
+			print r.text.split("\n",4)[4]
 
 	# ------------------------------------ Payment Log ---------------------------------
 
-	# List user's payment log (getpaymentlog username)
+	# List user's payment log
 	if req_type == 'getpaymentlog':
+		username = input('Username: ')
 
 		payload = {'req_type': req_type, 'username': username}
 		r = requests.post(url, data=payload)
 		if 'ERROR' in r.text:
 			print 'Incorrect username'
 		else:
-			print r.text
+			print r.text.split("\n",4)[4]
 
-	# Get all payments for a month (getmthpayment mmyy)
+	# Get all payments for a month
 	if req_type == 'getmthpayment':
-		mmyy = sys.argv[3]
+		mmyy = input('Month (MMYY): ')
 
 		payload = {'req_type': req_type, 'mmyy': mmyy}
 		r = requests.post(url, data=payload)
 		if 'ERROR' in r.text:
 			print 'Enter month in MMYY format (Jan 2017 as 0117)'
 		else:
-			print r.text
+			print r.text.split("\n",4)[4]
+
+
+	# ------------------------------------- Others --------------------------------------
+
+	# Send email reminder to client
+	if req_type == 'send_mail':
+		username = input('Username: ')
+		
+		payload = {'req_type': req_type, 'username': username}
+		r = requests.post(url, data=payload)
+		if 'ERROR' in r.text:
+			print 'No such user exists'
+		else:
+			print 'Email sent to user '+username
+
 
 if __name__ == '__main__':
     main()
